886a1430df738790d4f614e8b06754185844d785
dabirax <oliverpraise1@gmail.com>
2025-06-07 18:19:38 +0100

    FAQ

--- 03-reviews/starter/src/App.jsx
+++ 03-reviews/starter/src/App.jsx
@@ -1,4 +1,67 @@
+import { useState } from "react";
+import people from "./data";
+import { FaQuoteRight, FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const randomPerson = () => {
+    let random = Math.floor(Math.random() * people.length);
+    if (random === index) {
+      random = index + 1;
+    }
+    console.log(random);
+    setIndex(checkNumber(random));
+  };
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+        <button className="random-btn" onClick={randomPerson}>
+          Suprise me
+        </button>
+      </div>
+    </article>
+  );
 };
 export default App;
--- 04-accordion/starter/src/App.jsx
+++ 04-accordion/starter/src/App.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+import data from './data'
+import SingleQuestion from "./Question"
+
 const App = () => {
-  return <h2>Accordion Starter</h2>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+        <section className="info">{questions.map((question)=>{
+          return <SingleQuestion key={question.id} {...question}/>
+        })}</section>
+      </div>
+    </main>
+  );
 };
 export default App;
