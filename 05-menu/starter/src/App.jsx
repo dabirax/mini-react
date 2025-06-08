d6e85f9a1708a4e500feee3cb7f75c501d6bf197
dabirax <oliverpraise1@gmail.com>
2025-06-08 09:24:26 +0100

    Menu

--- 05-menu/starter/src/App.jsx
+++ 05-menu/starter/src/App.jsx
@@ -1,4 +1,34 @@
+import { useState } from "react";
+import items from "./data";
+import Categories from "./Categories";
+import Menu from "./Menu";
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => category === item.category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="title-underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 export default App;
