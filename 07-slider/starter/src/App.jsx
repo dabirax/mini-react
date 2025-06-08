7b86c8f3d2759ae3cec8073c28eb4d7e7321418f
dabirax <oliverpraise1@gmail.com>
2025-06-08 11:32:22 +0100

    commit

--- 07-slider/starter/src/App.jsx
+++ 07-slider/starter/src/App.jsx
@@ -1,8 +1,35 @@
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import { useState } from "react";
+import {list, longList, shortList} from "./data";
+
 const App = () => {
+  const [people, setPeople] = useState(list);
+  const [index, setIndex] = useState(2);
+
   return (
-    <main>
-      <h2>Slider Starter</h2>
-    </main>
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span> Reviews
+        </h2>
+      </div>
+      <div className="slider-container">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          return (
+            <article key={id} className="slide">
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon"></FaQuoteRight>
+            </article>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 export default App;
