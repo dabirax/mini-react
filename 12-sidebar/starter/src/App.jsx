339343b8259a2452845db8317f66463c307efc5a
dabirax <oliverpraise1@gmail.com>
2025-06-30 23:54:32 +0100

    commit

--- 12-sidebar/starter/src/App.jsx
+++ 12-sidebar/starter/src/App.jsx
@@ -1,4 +1,14 @@
+import Modal from "../Modal";
+import Home from "./Home";
+import Sidebar from "./Sidebar";
+
 const App = () => {
-  return <h2>Sidebar Starter</h2>;
+  return (
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  );
 };
 export default App;
