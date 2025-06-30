123e859a3add8db40981095e53888d9dce4222e6
dabirax <oliverpraise1@gmail.com>
2025-06-30 19:45:17 +0100

    commit

--- 11-navbar/starter/src/App.jsx
+++ 11-navbar/starter/src/App.jsx
@@ -1,5 +1,4 @@
 import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
 
 const App = () => {
   return (
--- 11-navbar/starter/src/Navbar.jsx
+++ 11-navbar/starter/src/Navbar.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
 import logo from "./logo.svg";
-import { FaBars, FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    console.log(showLinks);
+    console.log(linksHeight);
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+    console.log(linksContainerRef.current.style.height);
+  }, [showLinks]);
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-        </div>
-        <div className=" item-container show-container">
-          <button className="nav-toggle">
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
@@ -27,7 +44,8 @@ const Navbar = () => {
             })}
           </ul>
         </div>
-      <ul className="social-icons"> 
+      </div>
+      <ul className="social-icons">
         {social.map((socialIcon) => {
           const { id, url, icon } = socialIcon;
           return (
@@ -37,8 +55,6 @@ const Navbar = () => {
           );
         })}
       </ul>
-      </div>
-
     </nav>
   );
 };
